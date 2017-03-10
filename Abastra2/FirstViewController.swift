7ad5993bc246b73f6d6b0f4655600cf3ee656558
Krystian Kulawiak <krystian-mac@macbook-pro-krystian.local>
2017-03-10 11:38:47 +0100

    Initial Commit

---
+++ Abastra2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Abastra2
+//
+//  Created by Krystian Kulawiak on 10.03.2017.
+//  Copyright © 2017 Krystian Kulawiak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Abastra2Tests/Abastra2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Abastra2Tests.swift
+//  Abastra2Tests
+//
+//  Created by Krystian Kulawiak on 10.03.2017.
+//  Copyright © 2017 Krystian Kulawiak. All rights reserved.
+//
+
+import XCTest
+@testable import Abastra2
+
+class Abastra2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
